0595432552be68450488abb66056bbe44f7ec586
Flo Faber <48768148+FloFaber@users.noreply.github.com>
2019-07-13 15:40:36 +0200

    Update localstorage.js

--- localstorage.js
+++ localstorage.js
@@ -3,5 +3,7 @@ window.storage: function(name, value){
 		localStorage.setItem(name, (is_array(value) || is_object(value) ? JSON.stringify(value) : value));return true;
 	}else if(typeof name !== "undefined" && !typeof value !== "undefined"){
 		if(localStorage.getItem(name)){try{return JSON.parse(localStorage.getItem(name))}catch(e){return localStorage.getItem(name)}}else{return false}
+	}else if(typeof name !== "undefined" && name === 0 && typeof value !== "undefined" && value === 0){
+		localStorage.clear();
 	}
 }
