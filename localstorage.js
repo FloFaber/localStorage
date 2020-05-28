a54e0460675a3c35c8a32a9d7e2f62d2a8e80e19
Flo Faber <48768148+FloFaber@users.noreply.github.com>
2020-05-28 08:36:49 +0200

    small bugfix

--- localstorage.js
+++ localstorage.js
@@ -1,6 +1,6 @@
-window.storage: function(name, value){
+window.storage = function(name, value){
 	if(typeof name !== "undefined" && typeof value !== "undefined"){
-		localStorage.setItem(name, (is_array(value) || is_object(value) ? JSON.stringify(value) : value));return true;
+		localStorage.setItem(name, (typeof value == "object" ? JSON.stringify(value) : value));return true;
 	}else if(typeof name !== "undefined" && !typeof value !== "undefined"){
 		if(localStorage.getItem(name)){try{return JSON.parse(localStorage.getItem(name))}catch(e){return localStorage.getItem(name)}}else{return false}
 	}else if(typeof name !== "undefined" && name === 0 && typeof value !== "undefined" && value === 0){
